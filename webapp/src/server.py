cbc49f4da16dae72b97d7082e27763d7570a835f
Viachaslau Viahera <viachaslau.viahera@effective-soft.com>
2022-06-28 12:31:47 +0300

    first page

--- webapp/src/server.py
+++ webapp/src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "This is the new page."
+    return "This is the first page."
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
