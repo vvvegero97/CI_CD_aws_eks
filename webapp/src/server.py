92ec7b1937222191e25f4b8bad72e67c6ff3ac8a
Viachaslau Viahera <viachaslau.viahera@effective-soft.com>
2022-06-30 10:42:31 +0300

    another page change

--- webapp/src/server.py
+++ webapp/src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "This is the second page."
+    return "This is the trird page."
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
